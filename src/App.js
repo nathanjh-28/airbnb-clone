b8c714b40e6dcc6e3975db2e898821cfee3c4897
nathanjh-28 <nathan@nathanjharris.com>
2022-06-17 13:23:52 -0500

    readying resource for first leg project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>this is the app component</h1>
+        </div>
+    )
+}
+
+export default App;
